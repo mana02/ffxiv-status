e615e3a108c93f1bf76c87b8b516e38ab10568f4
mana02 <68262907+mana02@users.noreply.github.com>
2026-04-08 01:37:17 +0900

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -4,18 +4,25 @@ import fs from "fs";
 const url = "https://jp.finalfantasyxiv.com/lodestone/worldstatus/";
 
 const browser = await puppeteer.launch({
-  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
 });
 
 const page = await browser.newPage();
-await page.goto(url, { waitUntil: "networkidle2" });
 
-// JS描画後のHTML取得
+await page.goto(url, {
+  waitUntil: "networkidle2",
+});
+
+// ★ ここ超重要
+await page.waitForSelector("table", { timeout: 15000 });
+
+// 念のため少し待つ
+await new Promise(r => setTimeout(r, 3000));
+
 const content = await page.content();
+
+fs.writeFileSync("debug.html", content);
 
 await browser.close();
 
-// とりあえず保存して中身確認
-fs.writeFileSync("debug.html", content);
-
-console.log("HTML saved.");
+console.log("DONE");
